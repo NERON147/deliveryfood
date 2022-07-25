47896dd7b7f848e8e3c7b249c59c07c441f29315
NERON147 <bezzubenkomaxim@gmail.com>
2022-07-25 22:47:26 +0300

    qq

---
+++ js/partners.js
@@ -0,0 +1,43 @@
+const cardsRestaurants = document.querySelector('.cardRestaurants')
+
+const renderItems = (data) => {
+    data.forEach(({ image, kitchen, name, price, products, stars, time_of_delivery }) => {
+        const a = document.createElement('a')
+    
+        a.setAttribute('href', '/restaurant.html')
+        a.classList.add('card')
+        a.classList.add('card-restaurants')
+        a.dataset.products = products
+
+        console.log(a.dataset.products)
+        a.innerHTML = 
+           ` <img src="${image}" alt="${name}" class="card-image" />
+						<div class="card-text">
+							<div class="card-heading">
+								<h3 class="card-title">${name}</h3>
+								<span class="card-tag tag">${time_of_delivery} мин</span>
+							</div>
+							<div class="card-info">
+								<div class="rating">
+									${stars}
+								</div>
+								<div class="price">${price}</div>
+								<div class="category">${kitchen}</div>
+							</div>
+						</div>
+            `
+						
+            cardsRestaurants.append(a)
+    })
+}
+
+
+fetch('https://diliveryfood-10397-default-rtdb.firebaseio.com/db/partners.json')
+.then((response) => response.json())
+.then((data) =>  {
+    renderItems(data)
+})
+
+.catch((error) => {
+    console.log(error)
+})
